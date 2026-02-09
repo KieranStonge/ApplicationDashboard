ff8974edef87f15c8d7166564b2293e08515fcfe
Kieran <kstonge@uncc.edu>
2026-02-09 15:13:25 -0500

    Add SQLite database and applications table

--- app/main.py
+++ app/main.py
@@ -1,5 +1,22 @@
+from database import get_connection
+
 def main():
-    print("Application Dashboard is running")
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS applications (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        company TEXT,
+        role TEXT,
+        stage TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+    print("Database Intialized")
 
 if __name__ == "__main__":
     main()
